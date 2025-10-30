e72626fe1708a5916143975542a3783e51ecdcb4
Nicplay1 <nicolasballesteros900@gmail.com>
2025-10-29 20:12:36 -0500

    Corrige ruta 2

--- app/app/settings.py
+++ app/app/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app.usuario',
-    'administrador',
+    'app.administrador',
     'residente',
     'vigilante',
     'crispy_forms',
